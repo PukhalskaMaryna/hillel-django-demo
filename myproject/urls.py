81d9c94cede06bc627646b0b2dc5fdc9a7c61b47
Pukhalska Maryna <pukhalsk@fuib.com>
2025-09-28 13:23:22 +0300

    feat(auth): pretty /token page + link from home

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,13 +4,18 @@ from django.contrib import admin
 from django.urls import path, include
 from books import views as books_views
 from django.views.generic import TemplateView
+from rest_framework.authtoken.views import obtain_auth_token  # додай імпорт
 
 urlpatterns = [
+    path('token/', books_views.token_page, name='token_page'),
     path('files/', books_views.files_list, name='files_list'),
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # ГОЛОВНА
-    path("books/", include("books.urls")),                                   # КАТАЛОГ НА /books/
-    path("api/", include("books.api")),                                       # API
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("books/", include("books.urls")),
+    path("api/", include("books.api")),
+
+    # зручно: POST {"username": "...", "password": "..."} -> {"token": "..."}
+    path("api/token/", obtain_auth_token),
 ]
 
 if settings.DEBUG:
